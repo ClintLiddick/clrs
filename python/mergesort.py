977e7f6464ea1e1be0420a5abe147c8a0676d540
Clint Liddick <clint.liddick@gmail.com>
2016-03-09 14:19:44 -0500

    working mergesort for simple case

--- python/mergesort.py
+++ python/mergesort.py
@@ -3,8 +3,8 @@
 import sys
 
 def merge(A,start,lower_end,end):
-    left = A[start:lower_end+1]
-    right = A[lower_end+1:end]
+    left = A[start:lower_end]
+    right = A[lower_end:end]
     left.append(sys.maxsize)
     right.append(sys.maxsize)
     i = 0
@@ -18,4 +18,17 @@ def merge(A,start,lower_end,end):
             j += 1
     return A
 
-        
+def sort(A,start=None,end=None):
+    if start is None:
+        start = 0
+    if end is None:
+        end = len(A)
+
+    if end <= start + 1:
+        return
+    else:
+        mid = (start + end) / 2
+        sort(A,start,mid)
+        sort(A,mid,end)
+        merge(A,start,mid,end)
+        return
